c2c8db8331ae18b2ceaea13c5897cd2016d04fde
roose <roose@mail.ru>
2017-09-10 15:25:26 +0600

    Add dist folder from app branch

--- js/app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('svgLogos', []);
-
-app.controller('ListController', function($scope, $http) {
-    $http.get('logos.json')
-        .then(function(res) {
-            $scope.logos = res.data;
-        });
-});
